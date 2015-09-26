87fed512eada05ee4324c984161cf01ecfa58d04
Atticus Liu <atticusacl@gmail.com>
2015-09-26 15:44:51 -0700

    comments

---
+++ app.js
@@ -0,0 +1,9 @@
+/*
+MOOD		SONG						ENDPOINT
+
+Happy		Good Life					https://api.spotify.com/v1/tracks/1fLdeDTrJWNkwOeFyAVLvF
+Sad			Bound 2						https://api.spotify.com/v1/tracks/3sNVsP50132BTNlImLx70i
+Mad			Blood on the Leaves			https://api.spotify.com/v1/tracks/42GcjriRK6srwHkfbkBqVl
+Kanye		I am a Good					https://api.spotify.com/v1/tracks/0HMCdlOPJkAGlB0MlSWIcO
+
+*/
